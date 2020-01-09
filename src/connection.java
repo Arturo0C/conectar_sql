adeb153b080f720339169be53968d0188234c1cd
Arturo0C <arturobuendia3@gmail.com>
2020-01-09 20:18:16 +0100

    Initial commit

---
+++ src/connection.java
@@ -0,0 +1,31 @@
+import java.sql.*;
+
+public class connection {
+    public static void main(String[] args) {
+
+        try {
+            String url = "jdbc:mysql://localhost:3306/sports";
+            Connection conexion =  DriverManager.getConnection(url,"admin","Nochelarga123-");
+
+            String query = "SELECT * FROM participant";
+
+            // Creamos java attement
+            Statement st = conexion.createStatement();
+
+            //Execute the query and get a java resulset
+            ResultSet rs = st.executeQuery(query);
+
+            while (rs.next()) {
+                // Mostramos lo que queremos de la query hecha.
+                System.out.println(rs.getString("name") + " " + rs.getString("dni"));
+
+            }
+            st.close();
+
+        } catch(Exception e) {
+            System.err.println(e.getMessage());
+        }
+
+
+    }
+}
