9dda918610ee2b7cf28fde118459c17fcfdba749
Arturo0C <58225061+Arturo0C@users.noreply.github.com>
2020-01-10 11:36:11 +0100

    1136

--- src/Insert.java
+++ src/Insert.java
@@ -1,17 +1,18 @@
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.Scanner;
 
-public class connection {
+public class Insert {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Numero de DNI: ");
-        String dni = sc.next();
 
         try {
             String url = "jdbc:mysql://localhost:3306/sports";
             Connection conexion =  DriverManager.getConnection(url,"root","tuenti1997");
 
-            String query = "SELECT * FROM `participant` WHERE `dni`='"+dni+"'";
+            String query = "SELECT * FROM `participant`";
 
             // Creamos java attement
             Statement st = conexion.createStatement();
@@ -19,26 +20,18 @@ public class connection {
             //Execute the query and get a java resulset
             ResultSet rs = st.executeQuery(query);
 
-            // Mostramos lo que queremos de la query hecha.
             while (rs.next()) {
-
-                String name = rs.getString("name");
-                String last_name1 = rs.getString("last_name1");
-                String last_name2 = rs.getString("last_name2");
-
-
-                int back_number = rs.getInt("back_number");
-
+                // Mostramos lo que queremos de la query hecha.
+                System.out.println("Nombre del jugador: "+rs.getString("name") + " " + rs.getString("last_name1")+ " " + rs.getString("last_name2") + ". Dorsal: " + rs.getInt("back_number"));
 
             }
             st.close();
             rs.close();
 
         } catch(Exception e) {
-            System.err.println("El programa no funciona.");
+            System.err.println(e.getMessage());
         }
 
 
     }
 }
-
