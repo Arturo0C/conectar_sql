d6fac11132c7fdd889a863f91189c43a46a300b8
Arturo0C <58225061+Arturo0C@users.noreply.github.com>
2020-01-10 12:40:28 +0100

    1240

--- src/connection.java
+++ src/connection.java
@@ -1,53 +1,109 @@
+
 import java.sql.*;
 import java.util.Scanner;
 
 public class connection {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Numero de DNI: ");
-        String dni = sc.next();
 
-        try {
-            String url = "jdbc:mysql://localhost:3306/sports";
-            Connection conexion =  DriverManager.getConnection(url,"root","tuenti1997");
+        System.out.println("Que quieres consultar?");
+        System.out.println("-----------------------");
+        System.out.println("1.personas");
+        System.out.println("2.equipos");
+        System.out.println("3.Listado de deportes");
+        int select = sc.nextInt();
+        System.out.println("");
 
-            String query = "SELECT * FROM `participant` WHERE `dni`='"+dni+"'";
+        switch (select) {
+            case 1: //Participants
+                System.out.println("Numero de DNI: ");
+                String dni = sc.next();
+                System.out.println("-----------------------");
 
-            // Creamos java attement
-            Statement st = conexion.createStatement();
+                try {
+                    String url = "jdbc:mysql://localhost:3306/sports";
+                    Connection conexion = DriverManager.getConnection(url, "root", "tuenti1997");
 
-            //Execute the query and get a java resulset
-            ResultSet rs = st.executeQuery(query);
+                    String query = "SELECT * FROM `participant` WHERE `dni`='" + dni + "'";
 
-            // Mostramos lo que queremos de la query hecha.
-            while (rs.next()) {
+                    // Creamos java attement
+                    Statement st = conexion.createStatement();
 
-                String name = rs.getString("name");
-                String last_name1 = rs.getString("last_name1");
-                String last_name2 = rs.getString("last_name2");
-                String nation = rs.getString("nationality");
-                int age = rs.getInt("age");
-                String birth_date = rs.getString("birth_date");
-                String gende = rs.getString("gender");
-                int height = rs.getInt("height");
-                double weight = rs.getDouble("weight");
-                String type = rs.getString("type");
-                String active = rs.getString("is_active");
-                int back_number = rs.getInt("back_number");
+                    //Execute the query and get a java resulset
+                    ResultSet rs = st.executeQuery(query);
 
-                System.out.println("Name: "+name+" "+last_name1+" "+last_name2+".");
+                    // Mostramos lo que queremos de la query hecha.
+                    while (rs.next()) {
 
+                        String name = rs.getString("name");
+                        String last_name1 = rs.getString("last_name1");
+                        String last_name2 = rs.getString("last_name2");
+                        String nation = rs.getString("nationality");
+                        int age = rs.getInt("age");
+                        String birth_date = rs.getString("birth_date");
+                        String gender = rs.getString("gender");
+                        int height = rs.getInt("height");
+                        double weight = rs.getDouble("weight");
+                        String type = rs.getString("type");
+                        String active = rs.getString("is_active");
+                        int back_number = rs.getInt("back_number");
 
+                        System.out.println("Name: " + name + " " + last_name1 + " " + last_name2 + ".");
+                        System.out.println("Nationality: " + nation);
+                        System.out.println("Age: " + age);
+                        System.out.println("Birth date: " + birth_date);
+                        System.out.println("Gender: " + gender);
+                        System.out.println("Height: " + height);
+                        System.out.println("Weight: " + weight);
+                        System.out.println("Active: " + active);
+                        System.out.println("Type: " + type);
+                        System.out.println("Back numner: " + back_number);
 
-            }
-            st.close();
-            rs.close();
+                    }
+                    System.out.println("-----------------------");
+                    st.close();
+                    rs.close();
 
-        } catch(Exception e) {
-            System.err.println("El programa no funciona.");
+                } catch (Exception e) {
+                    System.err.println("Fail to connect.");
+                }
+                break;
+            case 2: //Participants
+                System.out.println("Id team: ");
+                int id = sc.nextInt();
+                System.out.println("-----------------------");
+
+                try {
+                    String url = "jdbc:mysql://localhost:3306/sports";
+                    Connection conexion = DriverManager.getConnection(url, "root", "tuenti1997");
+
+                    String query = "SELECT * FROM `team` WHERE `id_team`='" + id + "'";
+
+                    // Creamos java attement
+                    Statement st = conexion.createStatement();
+
+                    //Execute the query and get a java resulset
+                    ResultSet rs = st.executeQuery(query);
+
+                    // Mostramos lo que queremos de la query hecha.
+                    while (rs.next()) {
+
+                        String name = rs.getString("name");
+                        String country = rs.getString("country");
+
+                        System.out.println("Name: " + name);
+                        System.out.println("Country: " + country);
+
+                    }
+                    System.out.println("-----------------------");
+                    st.close();
+                    rs.close();
+
+                } catch (Exception e) {
+                    System.err.println("Fail to connect.");
+                }
+                break;
         }
-
-
     }
 }
 
