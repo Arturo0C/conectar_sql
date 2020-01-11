eac904bde49312f70cfcb0da0291de888966419e
Arturo0C <58225061+Arturo0C@users.noreply.github.com>
2020-01-11 18:44:41 +0100

    v0.35

--- src/connector.java
+++ src/connector.java
@@ -7,7 +7,7 @@ public class connector {
     private static String str;
 
     public static void main(String[] args) throws SQLException {
-        insert();
+        partInsert();
     }
 
     // <--------------------------> Consult <-------------------------->
@@ -303,44 +303,49 @@ public class connector {
     static void partInsert() {
 
         Scanner sc = new Scanner(System.in);
-        System.out.println("National Document Number: ");
-        String dni = sc.next();
-        System.out.println("-----------------------");
-        if (comp_dni(dni)) {
             try {
                 String url = "jdbc:mysql://localhost:3306/sports";
                 Connection conexion = DriverManager.getConnection(url, "root", "tuenti1997");
+                conexion.setAutoCommit(false);
+
                 Statement st = conexion.createStatement();
 
-
+                System.out.println("DNI: ");
+                String dni = sc.next();
                 System.out.println("Name: ");
-                String name = sc.next("name");
+                String name = sc.next();
+                System.out.println("First last name: ");
+                String last_name1 = sc.next();
+                System.out.println("Second last name: ");
+                String last_name2 = sc.next();
                 System.out.println("Nationality: ");
                 String nationality = sc.next();
                 System.out.println("Age: ");
+                String age = sc.next();
                 System.out.println("Birth date: ");
+                String birth_date = sc.next();
                 System.out.println("Gender: ");
+                String gender = sc.next();
                 System.out.println("Height: ");
+                String height = sc.next();
                 System.out.println("Weight: ");
+                String weight = sc.next();
                 System.out.println("Active: ");
+                String active = sc.next();
                 System.out.println("Type: ");
+                String type = sc.next();
                 System.out.println("Back numner: ");
+                String back_number = sc.next();
 
-
+                st.executeUpdate("Insert into team (name,country) values ('"+name+"','"+nationality+"')");
+//                st.executeUpdate("INSERT INTO participant (back_number,name,last_name1,last_name2,age,gender,dni,nationality,height,weight,type,is_active, birth_date) VALUES ("+back_number+",'"+name+"'"+",'"+last_name1+"'"+",'"+last_name2+"'"+","+age+","+",'"+gender+"'"+",'"+dni+"'"+",'"+nationality+"'"+","+height+","+weight+",'"+type+"'"+",'"+active+"'"+",'"+birth_date+"')");
+                conexion.commit();
+                conexion.close();
 
 
             } catch (Exception e) {
                 System.err.println("Error");
             }
-                 } else {
-                    System.out.println("The National Identification Document introduced does not exist, do you want to try another one?");
-                    System.out.println("Yes(y) or not(n)");
-                    String i = sc.next();
-                    System.out.println("");
-                    if (i.equals("y")) {
-                        partInsert();
-                    }
-                 }
 
 
     }
